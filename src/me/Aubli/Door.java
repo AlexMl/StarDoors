e51529320286e88cceaaa31b785c8a3771e50bf4
AlexMl <alexandermlinzk@gmail.com>
2014-02-12 20:19:47 +0100

    Add door and DoorManager code

--- src/me/Aubli/Door.java
+++ src/me/Aubli/Door.java
@@ -1,5 +1,35 @@
 package me.Aubli;
 
-public class Door {
+import java.io.File;
 
+import org.bukkit.Location;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+public class Door {
+	
+	private File doorFile;
+	private FileConfiguration doorConfig;
+	
+	private int doorID;
+	private Location corner1;
+	private Location corner2;
+	
+	
+	public Door(Location corner1, Location corner2, int doorID, String doorPath) throws Exception{
+		this.corner1 = corner1;
+		this.corner2 = corner2;
+		
+		this.doorID = doorID;
+		
+		this.doorFile = new File(doorPath + doorID + ".yml");
+		
+		if(doorFile.exists()){
+			throw new Exception("doorID already exists!");
+		}
+		doorConfig = YamlConfiguration.loadConfiguration(doorFile);
+		
+	}
+	
+	
 }
--- src/me/Aubli/DoorManager.java
+++ src/me/Aubli/DoorManager.java
@@ -1,5 +1,44 @@
 package me.Aubli;
 
-public class DoorManager {
+import java.util.ArrayList;
 
+import org.bukkit.Location;
+
+public class DoorManager {
+	
+	private ArrayList<Door> doors = new ArrayList<Door>();
+	private String doorPath = StarDoor.doorPath;
+	
+	
+	
+	
+	public Door[] getDoors(){
+		Door[] doorArray = new Door[doors.size()];
+		
+		for(int i=0;i<doorArray.length;i++){
+			doorArray[i] = doors.get(i);
+		}
+		
+		return doorArray;
+	}
+	
+	public Door getDoor(){
+		return null;
+	}
+	
+	public boolean addDoor(Location corner1, Location corner2){
+		try {
+			Door door = new Door(corner1, corner2, 1, doorPath);
+			doors.add(door);
+		} catch (Exception e) {		
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public boolean removeDoor(){
+		
+		return false;
+	}
+	
 }
